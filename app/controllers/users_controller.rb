b18cdcd561b5a518069bfbea1423d1dc7b28c3fc
日野恵太 <keitahino0415@gmail.com>
2018-11-13 12:40:22 +0900

    7章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  def show
+    @user = User.find(params[:id])
+
+    # puts "😄#{params[:id]}"
+  end
+
+  def create
+   @user = User.new(user_params)
+   if @user.save
+     # 保存の成功をここで扱う。
+   else
+     render 'new'
+   end
+ end
+
+ private
+
+   def user_params
+     params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+   end
 end
